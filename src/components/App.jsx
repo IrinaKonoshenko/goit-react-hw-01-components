87b3153ce68f14cbd9a2fe8e7244878e4f4e9895
Irina Konoshenko <ayren.syssoyeva@gmail.com>
2022-11-17 19:52:17 +0600

    version 1

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import { Profile } from "./Profile/Profile";
+import { Statistics } from "./Statistics/Statistics";
+import { FriendList } from "./FriendList/FriendList";
+import user from '../user.json';
+import data from '../data.json';
+import friends from '../friends.json';
+
+export const App = () => {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics
+        title="Upload stats"
+        stats={data}
+      />
+      <FriendList
+        friends={friends}
+      />
+    </div>
+  )
+};
+
+
---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,15 @@
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className="friend-list">
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
